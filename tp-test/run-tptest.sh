dfd445e4916534387dc74db10c228fc8c91795f6
tangenta <tangenta@126.com>
2020-12-25 10:49:57 +0800

    integrate all the command into one script

---
+++ tp-test/run-tptest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -euo
+
+export TPBIN='/home/tangenta/gopath/src/github.com/pingcap/go-randgen/tp-test/tp-test'
+export SCENE='/home/tangenta/gopath/src/github.com/pingcap/go-randgen/tp-test/scenarios/clustered_index.yy'
+export DSN='root:@tcp(127.0.0.1:4000)/test'
+
+$TPBIN --store $DSN clear
+$TPBIN --store $DSN init
+$TPBIN --store $DSN gen --test 20
+$TPBIN --store $DSN run \
+--dsn1 'root:@tcp(127.0.0.1:4000)/?time_zone=UTC' \
+--dsn2 'root:@tcp(127.0.0.1:4001)/?time_zone=UTC'
+
+$TPBIN --store $DSN clear
